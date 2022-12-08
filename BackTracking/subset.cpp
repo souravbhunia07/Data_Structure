064a152297d30ccef5ffa250163de112e50a4375
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2022-12-08 23:57:54 +0530

    Update subset.cpp

--- BackTracking/subset.cpp
+++ BackTracking/subset.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+void findSubset(char *input, char *output, int i, int j)
+{
+    // Base case
+
+    if(input[i] == '\0')
+    {
+        output[j] = '\0';
+        if(output[0] == '\0')
+        {
+            cout << "NULL" << endl;
+        }
+        cout << output << endl;
+        return;
+    }
+
+    // Recursive case
+    // Including Ith letter
+
+    output[j] = input[i];
+    findSubset(input, output, i + 1, j + 1);
+
+    // Excluding the Ith letter
+    // Overwritting
+
+    findSubset(input, output, i + 1, j);
+}
+
+int main()
+{
+    char input[100];
+    char output[100];
+    cin >> input;
+    findSubset(input, output, 0, 0);
+    return 0;
+}
