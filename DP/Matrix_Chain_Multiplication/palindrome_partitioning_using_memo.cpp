eab0ee2630cca7a0b20b30f35ddf7050aa6571cf
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2022-12-17 11:32:43 +0530

    Update palindrome_partitioning_using_memo.cpp

--- DP/Matrix_Chain_Multiplication/palindrome_partitioning_using_memo.cpp
+++ DP/Matrix_Chain_Multiplication/palindrome_partitioning_using_memo.cpp
@@ -42,7 +42,26 @@ int solve(string s, int i, int j)
     int mn = INT32_MAX;
     for(int k = i; k < j; k++)
     {
-        int temp = 1 + solve(s, i, k) + solve(s, k + 1, j);
+        int left, right;
+        if(t[i][k] != -1)
+        {
+            left = t[i][k];
+        }
+        else
+        {
+            left = solve(s, i, k);
+            t[i][k] = left;
+        }
+        if(t[k+1][j] != -1)
+        {
+            right = t[k+1][j];
+        }
+        else
+        {
+            right = solve(s, k + 1, j);
+            t[k+1][j] = right;
+        }
+        int temp = 1 + left + right;
         if(temp < mn)
         {
             mn = temp;
