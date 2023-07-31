af3117bbb0c7d46635047fe2c398b90c4fd44277
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2023-07-31 18:19:49 +0530

    Create Cycle_detection_in_directed_graph.cpp

---
+++ graphs/Cycle_detection_in_directed_graph.cpp
@@ -0,0 +1,39 @@
+bool checkCycle(int node, vector<int> adj[], int vis[], int dfsVis[])
+    {
+        vis[node] = 1;
+        dfsVis[node] = 1;
+        for(auto it : adj[node])
+        {
+            if(!vis[it])
+            {
+                if(checkCycle(it, adj, vis, dfsVis))
+                {
+                    return true;
+                }
+            }
+            else if(dfsVis[it])
+            {
+                return true;
+            }
+        }
+        dfsVis[node] = 0;
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<int> adj[]) {
+        int vis[V], dfsVis[V];
+        memset(vis, 0, sizeof(vis));
+        memset(dfsVis, 0, sizeof(vis));
+            
+        for(int i = 0; i < V; i++)
+        {
+            if(!vis[i])
+            {
+                if(checkCycle(i, adj, vis, dfsVis))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
